bc816501fa42211222042c7521d322a9bda47115
Adlet Yermukhan <adletermukhan@gmail.com>
2026-02-16 12:14:35 +0500

    feat: implement auth-service

---
+++ src/app/layout/login/login.ts
@@ -0,0 +1,51 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+
+  imports: [
+    ReactiveFormsModule,
+
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+  ],
+
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class Login {
+  private fb = inject(FormBuilder);
+  private auth = inject(AuthService);
+
+  loading = signal(false);
+
+  form = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  submit() {
+    if (this.form.invalid) return;
+
+    const { username, password } = this.form.value;
+    if (!username || !password) return;
+
+    this.loading.set(true);
+
+    this.auth.login(username, password);
+
+    this.loading.set(false);
+  }
+}
